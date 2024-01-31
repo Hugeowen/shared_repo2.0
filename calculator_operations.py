c28eb627f9c3a149d82a0edfc0c474001e11cf47
Hugeowen <157615824+Hugeowen@users.noreply.github.com>
2024-01-31 14:19:25 -0400

    Update calculator_operations.py

--- calculator_operations.py
+++ calculator_operations.py
@@ -1,3 +1,4 @@
+import math
 def add(a, b):
     return a + b
 
@@ -12,3 +13,12 @@ def divide(a, b):
         return a / b
     else:
         return "Error: Division by zero"
+
+def absolute_value(a):
+    return abs(a)
+
+def quadratic(a):
+    return a * a
+
+def square_root(a):
+    return math.sqrt(a)
